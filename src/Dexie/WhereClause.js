334b1d4506da6e87da8b8f05c9439d26a226afb0
Max White <mushishi78@gmail.com>
2021-06-21 08:32:38 +0100

    Add where clause methods

--- src/Dexie/WhereClause.js
+++ src/Dexie/WhereClause.js
@@ -1,7 +1,135 @@
+exports._above = function (lowerBound) {
+  return function (whereClause) {
+    return function () {
+      return whereClause.above(lowerBound)
+    }
+  }
+}
+
+exports._aboveOrEqual = function (lowerBound) {
+  return function (whereClause) {
+    return function () {
+      return whereClause.aboveOrEqual(lowerBound)
+    }
+  }
+}
+
+exports._anyOf = function (keys) {
+  return function (whereClause) {
+    return function () {
+      return whereClause.anyOf(keys)
+    }
+  }
+}
+
+exports._anyOfIgnoreCase = function (keys) {
+  return function (whereClause) {
+    return function () {
+      return whereClause.anyOfIgnoreCase(keys)
+    }
+  }
+}
+
+exports._below = function (upperBound) {
+  return function (whereClause) {
+    return function () {
+      return whereClause.below(upperBound)
+    }
+  }
+}
+
+exports._belowOrEqual = function (upperBound) {
+  return function (whereClause) {
+    return function () {
+      return whereClause.belowOrEqual(upperBound)
+    }
+  }
+}
+
+exports._between = function (lowerBound) {
+  return function (upperBound) {
+    return function (includeLower) {
+      return function (includeUpper) {
+        return function (whereClause) {
+          return function () {
+            return whereClause.between(lowerBound, upperBound, includeLower, includeUpper)
+          }
+        }
+      }
+    }
+  }
+}
+
+exports._equals = function (key) {
+  return function (whereClause) {
+    return function () {
+      return whereClause.equals(key)
+    }
+  }
+}
+
+exports._equalsIgnoreCase = function (key) {
+  return function (whereClause) {
+    return function () {
+      return whereClause.equalsIgnoreCase(key)
+    }
+  }
+}
+
+exports._inAnyRange = function (ranges) {
+  return function (includeLowers) {
+    return function (includeUppers) {
+      return function () {
+        return whereClause.inAnyRange(ranges, { includeLowers: includeLowers, includeUppers: includeUppers })
+      }
+    }
+  }
+}
+
+exports._noneOf = function (keys) {
+  return function (whereClause) {
+    return function () {
+      return whereClause.noneOf(keys)
+    }
+  }
+}
+
+exports._notEqual = function (key) {
+  return function (whereClause) {
+    return function () {
+      return whereClause.notEqual(key)
+    }
+  }
+}
+
 exports._startsWith = function (prefix) {
   return function (whereClause) {
     return function () {
       return whereClause.startsWith(prefix)
+    }
+  }
+}
+
+exports._startsWithAnyOf = function (prefixes) {
+  return function (whereClause) {
+    return function () {
+      return whereClause.startsWithAnyOf(prefixes)
+    }
+  }
+}
+
+exports._startsWithIgnoreCase = function (prefix) {
+  return function (whereClause) {
+    return function () {
+      return whereClause.startsWithIgnoreCase(prefix)
+    }
+  }
+}
+
+exports._startsWithAnyOfIgnoreCase = function (prefixes) {
+  return function (whereClause) {
+    return function () {
+      return whereClause.startsWithAnyOfIgnoreCase(prefixes)
     }
   }
 }
--- src/Dexie/WhereClause.purs
+++ src/Dexie/WhereClause.purs
@@ -1,15 +1,98 @@
-module Dexie.WhereClause (module DataRexports, startsWith) where
+module Dexie.WhereClause (
+    module DataRexports,
+    above,
+    aboveOrEqual,
+    anyOf,
+    anyOfIgnoreCase,
+    below,
+    belowOrEqual,
+    between,
+    equals,
+    equalsIgnoreCase,
+    inAnyRange,
+    noneOf,
+    notEqual,
+    startsWith,
+    startsWithAnyOf,
+    startsWithIgnoreCase,
+    startsWithAnyOfIgnoreCase
+) where
 
 import Prelude
 
+import Data.Tuple (Tuple(..))
 import Dexie.Collection (Collection)
-import Dexie.Internal.Data (WhereClause) as DataRexports
 import Dexie.Internal.Data (WhereClause)
+import Dexie.Internal.Data (WhereClause) as DataRexports
 import Effect (Effect)
 import Effect.Class (class MonadEffect, liftEffect)
 
+foreign import _above :: forall v. v -> WhereClause -> Effect Collection
+foreign import _aboveOrEqual :: forall v. v -> WhereClause -> Effect Collection
+foreign import _anyOf :: forall v. Array v -> WhereClause -> Effect Collection
+foreign import _anyOfIgnoreCase :: Array String -> WhereClause -> Effect Collection
+foreign import _below :: forall v. v -> WhereClause -> Effect Collection
+foreign import _belowOrEqual :: forall v. v -> WhereClause -> Effect Collection
+foreign import _between :: forall v. v -> v -> Boolean -> Boolean -> WhereClause -> Effect Collection
+foreign import _equals :: forall v. v -> WhereClause -> Effect Collection
+foreign import _equalsIgnoreCase :: String -> WhereClause -> Effect Collection
+foreign import _inAnyRange :: forall v. Array (Array v) -> Boolean -> Boolean -> WhereClause -> Effect Collection
+foreign import _noneOf :: forall v. Array v -> WhereClause -> Effect Collection
+foreign import _notEqual :: forall v. v -> WhereClause -> Effect Collection
 foreign import _startsWith :: String -> WhereClause -> Effect Collection
+foreign import _startsWithAnyOf :: Array String -> WhereClause -> Effect Collection
+foreign import _startsWithIgnoreCase :: String -> WhereClause -> Effect Collection
+foreign import _startsWithAnyOfIgnoreCase :: Array String -> WhereClause -> Effect Collection
+
+above :: forall v me. MonadEffect me => v -> WhereClause -> me Collection
+above lowerBound whereClause = liftEffect $ _above lowerBound whereClause
+
+aboveOrEqual :: forall v me. MonadEffect me => v -> WhereClause -> me Collection
+aboveOrEqual lowerBound whereClause = liftEffect $ _aboveOrEqual lowerBound whereClause
+
+anyOf :: forall v me. MonadEffect me => Array v -> WhereClause -> me Collection
+anyOf values whereClause = liftEffect $ _anyOf values whereClause
+
+anyOfIgnoreCase :: forall me. MonadEffect me => Array String -> WhereClause -> me Collection
+anyOfIgnoreCase stringValues whereClause = liftEffect $ _anyOfIgnoreCase stringValues whereClause
+
+below :: forall v me. MonadEffect me => v -> WhereClause -> me Collection
+below upperBound whereClause = liftEffect $ _below upperBound whereClause
+
+belowOrEqual :: forall v me. MonadEffect me => v -> WhereClause -> me Collection
+belowOrEqual upperBound whereClause = liftEffect $ _belowOrEqual upperBound whereClause
+
+between :: forall v me. MonadEffect me => v -> v -> Boolean -> Boolean -> WhereClause -> me Collection
+between lowerBound upperBound includeLower includeUpper whereClause =
+    liftEffect $ _between lowerBound upperBound includeLower includeUpper whereClause
+
+equals :: forall v me. MonadEffect me => v -> WhereClause -> me Collection
+equals value whereClause = liftEffect $ _equals value whereClause
+
+equalsIgnoreCase :: forall me. MonadEffect me => String -> WhereClause -> me Collection
+equalsIgnoreCase value whereClause = liftEffect $ _equalsIgnoreCase value whereClause
+
+inAnyRange :: forall v me. MonadEffect me => Array (Tuple v v) -> Boolean -> Boolean -> WhereClause -> me Collection
+inAnyRange ranges includeLowers includeUppers whereClause =
+    liftEffect $ _inAnyRange (map fn ranges) includeLowers includeUppers whereClause
+  where
+    fn (Tuple f s) = [f, s]
+
+noneOf :: forall v me. MonadEffect me => Array v -> WhereClause -> me Collection
+noneOf values whereClause = liftEffect $ _noneOf values whereClause
+
+notEqual :: forall v me. MonadEffect me => v -> WhereClause -> me Collection
+notEqual value whereClause = liftEffect $ _notEqual value whereClause
 
 startsWith :: forall me. MonadEffect me => String -> WhereClause -> me Collection
 startsWith prefix whereClause = liftEffect $ _startsWith prefix whereClause
+
+startsWithAnyOf :: forall me. MonadEffect me => Array String -> WhereClause -> me Collection
+startsWithAnyOf prefixes whereClause = liftEffect $ _startsWithAnyOf prefixes whereClause
+
+startsWithIgnoreCase :: forall me. MonadEffect me => String -> WhereClause -> me Collection
+startsWithIgnoreCase prefix whereClause = liftEffect $ _startsWithIgnoreCase prefix whereClause
+
+startsWithAnyOfIgnoreCase :: forall me. MonadEffect me => Array String -> WhereClause -> me Collection
+startsWithAnyOfIgnoreCase prefixes whereClause = liftEffect $ _startsWithAnyOfIgnoreCase prefixes whereClause
 
